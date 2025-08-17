eca8565c86a083f087b6b656fe00b877e7da6891
Dmitry Shaporenkov <dshapor@fb.com>
2025-08-17 07:30:52 +0100

    Refactor conversion code a bit
    Summary:
    Refactor conversion code a bit

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ function convertInput(){
     const userInput = document.getElementById("userNum").value;
     const units = document.getElementById("dropdown").value;
     const output = document.getElementById("resultRect");
-    
+
     let conversion;
-    
-    if (units === "1") {
-        conversion = parseFloat(userInput) * 1.6;  
-    }   else if (units === "2") {
-        conversion = parseFloat(userInput) / 1.6;
-    }   else if (units === "3") {
-        conversion = parseFloat(userInput) * 2.54;
-    }   else {
-        conversion = parseFloat(userInput) / 2.54;
+    const MILE_TO_KM = 1.6;
+    const INCH_TO_CM = 2.54;
+
+    switch (units) {
+        case "1":
+            conversion = parseFloat(userInput) * MILE_TO_KM;
+            break;
+        case "2":
+            conversion = parseFloat(userInput) / MILE_TO_KM;
+            break;
+        case "3":
+            conversion = parseFloat(userInput) * INCH_TO_CM;
+            break;
+        default:
+            conversion = parseFloat(userInput) / INCH_TO_CM;
+            break;
     }
     output.textContent = `Result: ${conversion.toFixed(2)}`;
 }
